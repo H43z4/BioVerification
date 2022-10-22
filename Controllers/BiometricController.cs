2c572ffa1595a1a4956367b3d275b048a3fdd3c8
shehzaad.ahmad <https://gitlab.pitb.gov.pk/shehzaad.ahmad>
2022-10-22 16:16:58 +0500

    d

--- Controllers/BiometricController.cs
+++ Controllers/BiometricController.cs
@@ -2,10 +2,10 @@
 using Microsoft.AspNetCore.Mvc;
 using Models.ViewModels.Identity;
 using Biometric.Services;
-using Biometric.ViewModels;
 using SharedLib.APIs;
 using SharedLib.Common;
 using SharedLib.Security;
+using Models.ViewModels.Biometric;
 
 namespace Biometric.Controllers
 {
@@ -28,24 +28,117 @@ namespace Biometric.Controllers
             this.biometricService = biometricService;
         }
 
+        //[HttpGet(Name = "GetVehicleInfo")]
+        //public async Task<ApiResponse> GetVehicleInfo(IMSaveBiometricInfo inputModel)
+        //{
+        //    this.biometricService.VwUser = this.User;
+
+        //    var ds = await this.biometricService.GetVehicleInfo(inputModel);
+
+        //    return ApiResponse.GetApiResponse(ApiResponseType.FAILED, null, Constants.NOT_FOUND_MESSAGE);
+        //}
+
+        //[HttpPost]
+        //public async Task<ApiResponse> SaveBiometricInfo(VwBiometricInfo vwBiometricInfo)
+        //{
+        //    this.biometricService.VwUser = this.User;
+
+        //    var ds = await this.biometricService.SaveBiometricInfo(vwBiometricInfo);
+
+        //    return ApiResponse.GetApiResponse(ApiResponseType.FAILED, null, Constants.NOT_FOUND_MESSAGE);
+        //}
+
+
+        #region NADRA
+
+
         [HttpGet(Name = "GetVehicleInfo")]
-        public async Task<ApiResponse> GetVehicleInfo(InputModel inputModel)
+        public async Task<ApiResponse> GetVehicleInfo(VwVehicleInfoIM vwVehicleInfoIM)
         {
             this.biometricService.VwUser = this.User;
 
-            var ds = await this.biometricService.GetVehicleInfo(inputModel);
+            var ds = await this.biometricService.GetVehicleInfo(vwVehicleInfoIM);
 
-            return ApiResponse.GetApiResponse(ApiResponseType.FAILED, null, Constants.NOT_FOUND_MESSAGE);
+            var vehicleInfoList = ds.Tables[0].ToList<VwVehicleInfo>();
+
+            var vehicleInfo = vehicleInfoList.Count > 0 ? vehicleInfoList.FirstOrDefault() : null;
+
+            var apiResponseType = vehicleInfo is not null ? ApiResponseType.SUCCESS : ApiResponseType.FAILED;
+            var msg = vehicleInfo is not null ? Constants.RECORD_FOUND_MESSAGE : Constants.NOT_FOUND_MESSAGE;
+
+            return ApiResponse.GetApiResponse(apiResponseType, vehicleInfo, msg);
         }
 
         [HttpPost]
-        public async Task<ApiResponse> SaveBiometricInfo(InputModel inputModel)
+        public async Task<ApiResponse> SaveNadraFranchise(VwNadraFranchise vwNadraFranchise)
+        {
+            //this.biometricService.VwUser = this.User;
+
+            //var rowsAffected = await this.biometricService.SaveNadraFranchise(vwNadraFranchise);
+
+            //var apiResponseType = rowsAffected > 0 ? ApiResponseType.SUCCESS : ApiResponseType.FAILED;
+            //var msg = rowsAffected > 0 ? Constants.DATA_SAVED_MESSAGE : Constants.DATA_NOT_SAVED_MESSAGE;
+
+            //return ApiResponse.GetApiResponse(apiResponseType, null, msg);
+
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        public async Task<ApiResponse> SaveBiometricInfo(VwBiometricInfo vwBiometricInfo)
         {
             this.biometricService.VwUser = this.User;
 
-            var ds = await this.biometricService.SaveBiometricInfo(inputModel);
+            var ds = await this.biometricService.SaveBiometricInfo(vwBiometricInfo);
 
-            return ApiResponse.GetApiResponse(ApiResponseType.FAILED, null, Constants.NOT_FOUND_MESSAGE);
+            var apiResponseType = ApiResponseType.SUCCESS;
+            var msg = Constants.DATA_SAVED_MESSAGE;
+
+            if (ds.Tables[0].Rows[0][0].ToString() == "0")
+            {
+                apiResponseType = ApiResponseType.SUCCESS;
+                msg = Constants.DATA_SAVED_MESSAGE;
+            }
+            else
+            {
+                apiResponseType = ApiResponseType.FAILED;
+                msg = Constants.DATA_NOT_SAVED_MESSAGE;
+            }
+
+            return ApiResponse.GetApiResponse(apiResponseType, null, msg);
         }
+
+        #endregion
+
+        #region Organizational-Registration
+
+        [AllowAnonymous]
+        [HttpGet(Name = "GetAssociations")]
+        public async Task<ApiResponse> GetAssociations(string ntn)
+        {
+            //this.biometricService.VwUser = this.User;
+
+            //var ds = await this.biometricService.GetAssociations(ntn);
+
+            //var repAssociations = ds.Tables[0].ToList<VwRepAssociation>();
+            //var persons = ds.Tables[0].ToList<VwPerson>();  // dataset contains only one table. Tables[0]
+
+            //foreach (var rep in repAssociations)
+            //{
+            //    if (rep.PersonId.HasValue)
+            //    {
+            //        rep.Person = persons.SingleOrDefault(x => x.PersonId == rep.PersonId);
+            //    }
+            //}
+
+            //var apiResponseType = repAssociations is not null ? ApiResponseType.SUCCESS : ApiResponseType.FAILED;
+            //var msg = repAssociations is not null ? Constants.RECORD_FOUND_MESSAGE : Constants.NOT_FOUND_MESSAGE;
+
+            //return ApiResponse.GetApiResponse(apiResponseType, repAssociations, msg);
+
+            throw new NotImplementedException();
+        }
+
+        #endregion
     }
 }
--- Services/BiometricService.cs
+++ Services/BiometricService.cs
@@ -1,15 +1,16 @@
 ﻿using Database;
 using Models.ViewModels.Identity;
-using Biometric.ViewModels;
 using SharedLib.Interfaces;
 using System.Data;
+using Microsoft.Data.SqlClient;
+using Models.ViewModels.Biometric;
 
 namespace Biometric.Services
 {
     public interface IBiometricService : ICurrentUser
     {
-        Task<DataSet> GetVehicleInfo(InputModel inputModel);
-        Task<DataSet> SaveBiometricInfo(InputModel inputModel);
+        Task<DataSet> GetVehicleInfo(VwVehicleInfoIM vwVehicleInfoIM);
+        Task<DataSet> SaveBiometricInfo(VwBiometricInfo vwBiometricInfo);
     }
 
     public class BiometricService : IBiometricService
@@ -24,13 +25,13 @@ namespace Biometric.Services
 
         #region public-Methods
 
-        public async Task<DataSet> GetVehicleInfo(InputModel inputModel)
+
+        public async Task<DataSet> GetVehicleInfo(VwVehicleInfoIM vwVehicleInfoIM)
         {
             Dictionary<string, object> paramDict = new Dictionary<string, object>();
 
-            paramDict.Add("@RegNo", inputModel.RegNo);
-            paramDict.Add("@ChasisNo", inputModel.ChasisNo);
-            paramDict.Add("@EngineNo", inputModel.EngineNo);
+            paramDict.Add("@CNIC", vwVehicleInfoIM.RegNo);
+            paramDict.Add("@ApplicationId", vwVehicleInfoIM.MvrsTransId);
             paramDict.Add("@UserId", this.VwUser.UserId);
 
             var ds = await this.dbHelper.GetDataSetByStoredProcedure("[Biometric].[GetVehicleInfo]", paramDict);
@@ -38,13 +39,16 @@ namespace Biometric.Services
             return ds;
         }
         
-        public async Task<DataSet> SaveBiometricInfo(InputModel inputModel)
+        public async Task<DataSet> SaveBiometricInfo(VwBiometricInfo vwBiometricInfo)
         {
             Dictionary<string, object> paramDict = new Dictionary<string, object>();
 
-            paramDict.Add("@RegNo", inputModel.RegNo);
-            paramDict.Add("@ChasisNo", inputModel.ChasisNo);
-            paramDict.Add("@EngineNo", inputModel.EngineNo);
+            paramDict.Add("@ApplicationId", vwBiometricInfo.MvrsTransId);
+            paramDict.Add("@CNIC", vwBiometricInfo.CNIC);
+            paramDict.Add("@RegNo", vwBiometricInfo.RegNo);
+            paramDict.Add("@NadraTransId", vwBiometricInfo.NadraTransId);
+            paramDict.Add("@NadraFranchiseId", vwBiometricInfo.NadraFranchiseId);
+            paramDict.Add("@IsVerified", vwBiometricInfo.IsVerified);
             paramDict.Add("@UserId", this.VwUser.UserId);
 
             var ds = await this.dbHelper.GetDataSetByStoredProcedure("[Biometric].[SaveBiometricInfo]", paramDict);
